1e41f0197694c16aeab46f4b586dd839f5437fa2
Ramnath Sharma <ramsharma.axel@gmail.com>
2022-06-04 20:37:56 +0530

    few changes

--- makeRequest.py
+++ makeRequest.py
@@ -7,7 +7,9 @@ nav_link = os.getenv("NAV_LINK")
 
 import requests as req
 response = req.get(nav_link)
-print(response.text[:100])
+# fundlist = response.text.split('\n')[:10]
+# for i in range(10):
+#     print(fundlist[i])
 with open('NAV.txt', 'w', encoding="utf-8") as f:
     f.write(response.text)
     f.close()
